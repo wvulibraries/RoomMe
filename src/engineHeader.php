16ce37c08ba82cc3eca3ec4064db9518f3b47e17
Michael Bond <mbond@th-forgotten.org>
2014-06-01 15:12:50 -0400

    updated includes for EngineAPI 4.0

--- src/engineHeader.php
+++ src/engineHeader.php
@@ -1,6 +1,10 @@
 <?php
 
-require_once "/home/library/public_html/includes/engineHeader.php";
+require_once '/home/www.libraries.wvu.edu/phpincludes/engine/engineAPI/4.0/engine.php';
+$engine = EngineAPI::singleton();
+errorHandle::errorReporting(errorHandle::E_ALL);
+
+// require_once "/home/library/public_html/includes/engineHeader.php";
 
 recurseInsert("acl.php","php"); 
 
