3b89cc26b365a0214dd2c22484c00aa4367d3169
Michael Bond <mbond@th-forgotten.org>
2014-06-03 22:12:56 -0400

    Engine Migration: updated database call and template call

--- src/admin/engineHeader.php
+++ src/admin/engineHeader.php
@@ -4,9 +4,15 @@ require_once("/home/library/public_html/includes/engineHeader.php");
 
 recurseInsert("acl.php","php"); 
 
-$engine->dbConnect("database","roomReservations",TRUE);
+$options = array(
+	'username' => 'username',
+	'password' => 'password',
+	'dbName'   => 'roomReservations',
+);
+require_once '/home/www.libraries.wvu.edu/phpincludes/databaseConnectors/database.lib.wvu.edu.remote.php';
+$db = db::create('mysql', $options, 'appDB');
 
-$engine->eTemplate("load","library2012.2col");
+templates::load("library2012.2col");
 
 $localvars->set("roomResBaseDir","/services/rooms");
 ?>
