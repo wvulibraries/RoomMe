edb6e8d6c6d3f804b4f4f834222d88c000c3cada
Michael Bond <mbond@the-forgotten.org>
2014-07-02 09:43:42 -0400

    temp acl settings

--- src/admin/acl.php
+++ src/admin/acl.php
@@ -2,10 +2,12 @@
 
 $engine = EngineAPI::singleton();
 
-$engine->accessControl("ADgroup","libraryWeb_roomReservation",TRUE,FALSE);
-$engine->accessControl("ADgroup","libraryWeb_roomReservation_rooms",TRUE,FALSE);
-$engine->accessControl("ADgroup","libraryWeb_roomReservation_admin",TRUE,FALSE);
-$engine->accessControl("denyAll",null,null);
+// $engine->accessControl("ADgroup","libraryWeb_roomReservation",TRUE,FALSE);
+// $engine->accessControl("ADgroup","libraryWeb_roomReservation_rooms",TRUE,FALSE);
+// $engine->accessControl("ADgroup","libraryWeb_roomReservation_admin",TRUE,FALSE);
+// $engine->accessControl("denyAll",null,null);
 
-$engine->accessControl("build");
+accessControl::accessControl("allowAll",null,null);
+
+accessControl::build();
 ?>
