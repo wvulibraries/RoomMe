1077d4950c08c46efae5bf9115053795b14acb63
Michael Bond <mbond@the-forgotten.org>
2014-07-02 09:43:06 -0400

    Engine4.0 Migration: Localvars and variables includes

--- src/admin/engineHeader.php
+++ src/admin/engineHeader.php
@@ -1,8 +1,15 @@
 <?php
 
-require_once("/home/library/public_html/includes/engineHeader.php");
+require_once '/home/www.libraries.wvu.edu/phpincludes/engine/engineAPI/4.0/engine.php';
+$engine = EngineAPI::singleton();
+errorHandle::errorReporting(errorHandle::E_ALL);
+
+// Set localVars and engineVars variables
+$localvars  = localvars::getInstance();
+$enginevars = enginevars::getInstance();
 
 recurseInsert("acl.php","php"); 
+recurseInsert("vars.php","php");
 
 $options = array(
 	'username' => 'username',
