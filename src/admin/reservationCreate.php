362f840f94cbc4d2fde8f6e368a994aabf8c834b
Michael Bond <mbond@the-forgotten.org>
2014-08-03 22:17:20 -0400

    Updated to use new class.

--- src/admin/reservationCreate.php
+++ src/admin/reservationCreate.php
@@ -1,7 +1,5 @@
 <?php
 require_once("engineHeader.php");
-recurseInsert("includes/functions.php","php");
-recurseInsert("includes/createReservations.php","php");
 
 $errorMsg = "";
 $error    = FALSE;
@@ -14,322 +12,291 @@ $reservationInfo = NULL;
 $username        = "";
 $groupname       = "";
 $comments        = "";
-$action          = "Add";
-if (isset($_GET['MYSQL']['id']) && validate::getInstance()->integer($_GET['MYSQL']['id']) === TRUE) {
-	$reservationID = $_GET['MYSQL']['id'];
-	$localvars->set("reservationID",$reservationID);
-	$sql       = sprintf("SELECT reservations.*, building.ID as buildingID FROM `reservations` LEFT JOIN `rooms` ON rooms.ID=reservations.roomID LEFT JOIN `building` ON building.ID=rooms.building WHERE reservations.ID=?");
-	$sqlResult = $db->query($sql,array($reservationID));
 
-	if ($sqlResult->error()) {
-		errorHandle::newError($sqlResult->errorMsg(), errorHandle::DEBUG);
-		$error = TRUE;
+$reservation = new Reservation;
+
+try {
+
+	// Is this an Update? 
+	if (isset($_GET['MYSQL']['id'])) {
+
+		if ($reservation->get($_GET['MYSQL']['id']) === FALSE) {
+			throw new Exception("Error retrieving reservation.");
+		}
+
 	}
 	else {
-		$reservationInfo = $sqlResult->fetch();
-		$username        = $reservationInfo['username'];
-		$groupname       = $reservationInfo['groupname'];
-		$comments        = $reservationInfo['comments'];
-		$_POST['MYSQL']['library'] = $reservationInfo['buildingID'];
-		$_POST['HTML']['library']  = $reservationInfo['buildingID'];
-		$_POST['RAW']['library']   = $reservationInfo['buildingID'];
-		$_POST['MYSQL']['room']    = $reservationInfo['roomID'];
-		$_POST['HTML']['room']     = $reservationInfo['roomID'];
-		$_POST['RAW']['room']      = $reservationInfo['roomID'];
+		if (!isset($_POST['MYSQL']['library']) && validate::getInstance()->integer($_POST['MYSQL']['library']) === FALSE) {
+			throw new Exception("Missing or invalid building");
+		}
+		if (!isset($_POST['MYSQL']['room']) && validate::getInstance()->integer($_POST['MYSQL']['room']) === FALSE) {
+			throw new Exception("Missing or invalid room");
+		}
 
-		$action = "Update";
+		$reservation->setBuilding($_POST['MYSQL']['library']);
+		$reservation->setRoom($_POST['MYSQL']['room']);
+
+	}
+
+	$localvars->set("buildingID",$reservation->building['ID']);
+	$localvars->set("roomID",$reservation->room['ID']);
+	$localvars->set("buildingName",$reservation->building['name']);
+	$localvars->set("roomName",$reservation->room['name']);
+	$localvars->set("roomNumber",$reservation->room['number']);
+
+
+	if (isset($_POST['MYSQL']['roomSubmit'])) {
+
+	// This comes from the room select page
+
+	}
+	else if (isset($_POST['MYSQL']['createSubmit'])) {
+
+		if (!$reservation->create()) {
+			throw new Exception("Error Creating Reservation.");
+		}
+
+	}
+	else if (isset($_POST['MYSQL']['deleteSubmit'])) {
+
+		if (!$reservation->delete()) {
+			throw new Exception("Error deleting reservation.");
+		}
+
+		// @TODO this should not be hard coded. 
+		header('Location: reservationsList.php');
+
 	}
 
 }
-
-if (!isset($_POST['MYSQL']['library']) && validate::getInstance()->integer($_POST['MYSQL']['library']) === FALSE) {
-	$errorMsg .= errorHandle::errorMsg("Missing or invalid building");
-	$error = TRUE;
-}
-if (!isset($_POST['MYSQL']['room']) && validate::getInstance()->integer($_POST['MYSQL']['room']) === FALSE) {
-	$errorMsg .= errorHandle::errorMsg("Missing or invalid room");
-	$error = TRUE;
+catch(Exception $e) {
+	errorHandle::errorMsg($e->getMessage());
 }
 
-
-
-if ($error === FALSE) {
-
-	$buildingID = $_POST['MYSQL']['library'];
-	$roomID     = $_POST['MYSQL']['room'];
-
-	$localvars->set("buildingID",$buildingID);
-	$localvars->set("roomID",$roomID);
-
-	$buildingName = getBuildingName($buildingID);
-	$roomName     = getRoomName($roomID);
-
-	$localvars->set("buildingName",$buildingName);
-	$localvars->set("roomName",$roomName);
-
+// Create the Via Dropdown
+try {
+	// @TODO : This needs to be taken out of here
 	$sql       = sprintf("SELECT * FROM `via` ORDER BY `name`");
 	$sqlResult = $db->query($sql);
 
 	if ($sqlResult->error()) {
 		errorHandle::newError($sqlResult->errorMsg(), errorHandle::DEBUG);
-		$error = TRUE;
+		throw new Exception("Error creating via select");
 	}
 	else {
 		$viaOptions = "";
 		while($row = $sqlResult->fetch()) {
 			$viaOptions .= sprintf('<option value="%s" %s>%s</option>',
 				htmlSanitize($row['ID']),
-				(!isnull($reservationInfo) && $row['ID'] == $reservationInfo['createdVia'])?"selected":"",
+				(!isnull($reservation->reservation) && $row['ID'] == $reservation->reservation['createdVia'])?"selected":"",
 				htmlSanitize($row['name'])
 				);
 		}
 		$localvars->set("viaOptions",$viaOptions);
 	}
-
+	// End Via TODO
+}
+catch (Exception $e) {
+	errorHandle::errorMsg($e->getMessage());
 }
 
-if (isset($_POST['MYSQL']['roomSubmit'])) {
+// Display time in 12 hour or 24 hour
+$displayHour = getConfig('24hour');
+$displayHour = ($displayHour != 1)?12:24;
 
-	// This comes from the room select page
+// If this is a new reservation, use the current time. 
+// If this is an update, use the time from the reservation
+$currentMonth = ($reservation->isNew())?date("n"):date("n",$reservation->reservation['startTime']);
+$currentDay   = ($reservation->isNew())?date("j"):date("j",$reservation->reservation['startTime']);
+$currentYear  = ($reservation->isNew())?date("Y"):date("Y",$reservation->reservation['startTime']);
+$currentHour  = ($reservation->isNew())?date("G"):date("G",$reservation->reservation['startTime']);
+$nextHour     = ($reservation->isNew())?(date("G")+1):date("G",$reservation->reservation['endTime']);
 
-}
-else if (isset($_POST['MYSQL']['createSubmit'])) {
-
-	createReservation($buildingID,$roomID);
-
-	if (isset($_GET['MYSQL']['id']) && validate::getInstance()->integer($_GET['MYSQL']['id']) === TRUE) {
-		$sql       = sprintf("SELECT reservations.*, building.ID as buildingID FROM `reservations` LEFT JOIN `rooms` ON rooms.ID=reservations.roomID LEFT JOIN `building` ON building.ID=rooms.building WHERE reservations.ID=?");
-		$sqlResult = $db->query($sql,array($reservationID));
-
-		if ($sqlResult->error()) {
-			errorHandle::newError($sqlResult->errorMsg(), errorHandle::DEBUG);
-			$error = TRUE;
-		}
-		else {
-			$reservationInfo = $sqlResult->fetch();
-			$username        = $reservationInfo['username'];
-			$groupname       = $reservationInfo['groupname'];
-			$comments        = $reservationInfo['comments'];
-		}
-
-	}
-	
-
-}
-else if (isset($_POST['MYSQL']['deleteSubmit'])) {
-
-	$sql       = sprintf("DELETE FROM `reservations` WHERE ID=?");
-	$sqlResult = $db->query($sql,array($_POST['MYSQL']['reservationID']));
-
-	if (!$sqlResult->error()) {
-		header('Location: reservationsList.php');
-	}
-
-	$error = TRUE;
-	errorHandle::newError($sqlResult->errorMsg(), errorHandle::DEBUG);
-	errorHandle::errorMsg("Error deleting reservation.");
-
-}
-
-$currentMonth = (isnull($reservationInfo))?date("n"):date("n",$reservationInfo['startTime']);
-$currentDay   = (isnull($reservationInfo))?date("j"):date("j",$reservationInfo['startTime']);
-$currentYear  = (isnull($reservationInfo))?date("Y"):date("Y",$reservationInfo['startTime']);
-$currentHour  = (isnull($reservationInfo))?date("G"):date("G",$reservationInfo['startTime']);
-$nextHour     = (isnull($reservationInfo))?(date("G")+1):date("G",$reservationInfo['endTime']);
-
-$localvars->set("username",$username);
-$localvars->set("groupname",$groupname);
-$localvars->set("comments",$comments);
-$localvars->set("action",$action);
-
-$sql        = sprintf("SELECT value FROM siteConfig WHERE name='24hour'");
-$sqlResult  = $db->query($sql);
-
-$displayHour = 24;
-if (!$sqlResult->error()) {
-	$row        = $sqlResult->fetch();
-	$displayHour = ($row['value'] == 1)?24:12;
-}
+// Set some localvars for use in the HTML below. 
+$localvars->set("username",($reservation->isNew())?"":$reservation->reservation['username']);
+$localvars->set("groupname",($reservation->isNew())?"":$reservation->reservation['groupname']);
+$localvars->set("comments",($reservation->isNew())?"":$reservation->reservation['comments']);
+$localvars->set("action",($reservation->isNew())?"Add":"Update");
 
 templates::display('header');
 ?>
 
 <header>
-<h1>{local var="action"} a Reservation</h1>
+	<h1>{local var="action"} a Reservation</h1>
 </header>
 
 <?php
 if (count($engine->errorStack) > 0) {
-?>
-<section id="actionResults">
-	<header>
-		<h1>Results</h1>
-	</header>
-	<?php print errorHandle::prettyPrint(); ?>
-</section>
-<?php } ?>
+	?>
+	<section id="actionResults">
+		<header>
+			<h1>Results</h1>
+		</header>
+		<?php print errorHandle::prettyPrint(); ?>
+	</section>
+	<?php } ?>
 
-<p>Adding a reservation for Room <strong>{local var="roomName"}</strong> in building <strong>{local var="buildingName"}</strong></p>
+	<p>Adding a reservation for Room <strong>{local var="roomName"} {local var="roomNumber"}</strong> in building <strong>{local var="buildingName"}</strong></p>
 
-<form action="{phpself query="true"}" method="post">
-	{csrf}
+	<form action="{phpself query="true"}" method="post">
+		{csrf}
 
-	<input type="hidden" name="library" value="{local var="buildingID"}" />
-	<input type="hidden" name="room" value="{local var="roomID"}" />
-	<input type="hidden" name="reservationID" value="{local var="reservationID"}" />
+		<input type="hidden" name="library" value="{local var="buildingID"}" />
+		<input type="hidden" name="room" value="{local var="roomID"}" />
+		<input type="hidden" name="reservationID" value="{local var="reservationID"}" />
 
-	<fieldset>
-		<legend>User Information</legend>
-		<label for="username" class="requiredField">Username:</label> &nbsp; <input type="text" id="username" name="username" value="{local var="username"}"/>
+		<fieldset>
+			<legend>User Information</legend>
+			<label for="username" class="requiredField">Username:</label> &nbsp; <input type="text" id="username" name="username" value="{local var="username"}"/>
+			<br />
+			<label for="groupName">Groupname:</label> &nbsp; <input type="text" id="groupname" name="groupname" value="{local var="groupname"}"/>
+		</fieldset>
 		<br />
-		<label for="groupName">Groupname:</label> &nbsp; <input type="text" id="groupname" name="groupname" value="{local var="groupname"}"/>
-	</fieldset>
-	<br />
-	<fieldset>
-		<legend>Room Information</legend>
-	<table>
-		<tr>
-			<td>
-				<label for="start_month">Month:</label><br />
-				<select name="start_month" id="start_month" >
-					<?php
-						
-						for($I=1;$I<=12;$I++) {
-							printf('<option value="%s" %s>%s</option>',
-								($I < 10)?"0".$I:$I,
-								($I == $currentMonth)?"selected":"",
-								$I);
-						}
-					?>
-				</select>
-			</td>
-				<td>
-				<label for="start_day">Day:</label><br />
-				<select name="start_day" id="start_day" >
-					<?php
+		<fieldset>
+			<legend>Room Information</legend>
+			<table>
+				<tr>
+					<td>
+						<label for="start_month">Month:</label><br />
+						<select name="start_month" id="start_month" >
+							<?php
 
-						for($I=1;$I<=31;$I++) {
-							printf('<option value="%s" %s>%s</option>',
-								($I < 10)?"0".$I:$I,
-								($I == $currentDay)?"selected":"",
-								$I);
-						}
-					?>
-				</select>
-			</td>
-			<td>
-				<label for="start_year">Year:</label><br />
-				<select name="start_year" id="start_year" >
-					<?php
+							for($I=1;$I<=12;$I++) {
+								printf('<option value="%s" %s>%s</option>',
+									($I < 10)?"0".$I:$I,
+									($I == $currentMonth)?"selected":"",
+									$I);
+							}
+							?>
+						</select>
+					</td>
+					<td>
+						<label for="start_day">Day:</label><br />
+						<select name="start_day" id="start_day" >
+							<?php
 
-						for($I=$currentYear;$I<=$currentYear+10;$I++) {
-							printf('<option value="%s">%s</option>',
-								$I,
-								$I);
-						}
-					?>
-				</select>
-			</td>
-			<td></td>
-		</tr>
-		<tr>
-			<td colspan="2">
-				Start Time
-			</td>
-			<td colspan="2">
-				End Time
-			</td>
-		</tr>
-		<tr>	
-			<td>
-				<label for="start_hour">Hour:</label><br />
-				<select name="start_hour" id="start_hour" >
-					<?php
+							for($I=1;$I<=31;$I++) {
+								printf('<option value="%s" %s>%s</option>',
+									($I < 10)?"0".$I:$I,
+									($I == $currentDay)?"selected":"",
+									$I);
+							}
+							?>
+						</select>
+					</td>
+					<td>
+						<label for="start_year">Year:</label><br />
+						<select name="start_year" id="start_year" >
+							<?php
 
-						for($I=0;$I<=23;$I++) {
-							printf('<option value="%s" %s>%s</option>',
-								($I < 10)?"0".$I:$I,
-								($I == $currentHour)?"selected":"",
-								($displayHour == 24)?$I:(($I==12)?"12pm":(($I>=13)?($I-12)."pm":(($I == 0)?"12am":$I."am"))));
-						}
-					?>
-				</select>
-			</td>
-			<td>
-				<label for="start_minute">Minute:</label><br />
-				<select name="start_minute" id="start_minute" >
-					<?php
-						for($I=0;$I<60;$I += 15) {
-							printf('<option value="%s">%s</option>',
-								($I < 10)?"0".$I:$I,
-								$I);
-						}
-					?>
-				</select>
-			</td>
+							for($I=$currentYear;$I<=$currentYear+10;$I++) {
+								printf('<option value="%s">%s</option>',
+									$I,
+									$I);
+							}
+							?>
+						</select>
+					</td>
+					<td></td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						Start Time
+					</td>
+					<td colspan="2">
+						End Time
+					</td>
+				</tr>
+				<tr>	
+					<td>
+						<label for="start_hour">Hour:</label><br />
+						<select name="start_hour" id="start_hour" >
+							<?php
 
-			<td>
-				<label for="end_hour">Hour:</label><br />
-				<select name="end_hour" id="end_hour" >
-					<?php
+							for($I=0;$I<=23;$I++) {
+								printf('<option value="%s" %s>%s</option>',
+									($I < 10)?"0".$I:$I,
+									($I == $currentHour)?"selected":"",
+									($displayHour == 24)?$I:(($I==12)?"12pm":(($I>=13)?($I-12)."pm":(($I == 0)?"12am":$I."am"))));
+							}
+							?>
+						</select>
+					</td>
+					<td>
+						<label for="start_minute">Minute:</label><br />
+						<select name="start_minute" id="start_minute" >
+							<?php
+							for($I=0;$I<60;$I += 15) {
+								printf('<option value="%s">%s</option>',
+									($I < 10)?"0".$I:$I,
+									$I);
+							}
+							?>
+						</select>
+					</td>
 
-						for($I=0;$I<=23;$I++) {
-							printf('<option value="%s" %s>%s</option>',
-								($I < 10)?"0".$I:$I,
-								($I == $nextHour)?"selected":"",
-								($displayHour == 24)?$I:(($I==12)?"12pm":(($I>=13)?($I-12)."pm":(($I == 0)?"12am":$I."am"))));
-						}
-					?>
-				</select>
-			</td>
-			<td>
-				<label for="end_minute">Minute:</label><br />
-				<select name="end_minute" id="end_minute" >
-					<?php
-						for($I=0;$I<60;$I += 15) {
-							printf('<option value="%s">%s</option>',
-								($I < 10)?"0".$I:$I,
-								$I);
-						}
-					?>
-				</select>
-			</td>
-		</tr>
-	</table>
-</fieldset>
-	<br />
-	<fieldset>
-		<legend>Administrative Information</legend>
-		<label for="via">Via:</label>
-		<select name="via" id="via">
-			{local var="viaOptions"}
-		</select>
+					<td>
+						<label for="end_hour">Hour:</label><br />
+						<select name="end_hour" id="end_hour" >
+							<?php
+
+							for($I=0;$I<=23;$I++) {
+								printf('<option value="%s" %s>%s</option>',
+									($I < 10)?"0".$I:$I,
+									($I == $nextHour)?"selected":"",
+									($displayHour == 24)?$I:(($I==12)?"12pm":(($I>=13)?($I-12)."pm":(($I == 0)?"12am":$I."am"))));
+							}
+							?>
+						</select>
+					</td>
+					<td>
+						<label for="end_minute">Minute:</label><br />
+						<select name="end_minute" id="end_minute" >
+							<?php
+							for($I=0;$I<60;$I += 15) {
+								printf('<option value="%s">%s</option>',
+									($I < 10)?"0".$I:$I,
+									$I);
+							}
+							?>
+						</select>
+					</td>
+				</tr>
+			</table>
+		</fieldset>
 		<br />
+		<fieldset>
+			<legend>Administrative Information</legend>
+			<label for="via">Via:</label>
+			<select name="via" id="via">
+				{local var="viaOptions"}
+			</select>
+			<br />
 
-		<label for="override">Override:</label>
-		<select name="override" id="override">
-			<option value="0" selected>No</option>
-			<option value="1">Yes</option>
-		</select>
-		<label for="comments">
-			Comments/Notes:
-		</label>
-		<textarea name="comments" id="comments">{local var="comments"}</textarea>
-	</fieldset>
-	<br /><br />
-	<input type="submit" name="createSubmit" value="Reserve this Room"/> &nbsp;&nbsp;
+			<label for="override">Override:</label>
+			<select name="override" id="override">
+				<option value="0" selected>No</option>
+				<option value="1">Yes</option>
+			</select>
+			<label for="comments">
+				Comments/Notes:
+			</label>
+			<textarea name="comments" id="comments">{local var="comments"}</textarea>
+		</fieldset>
+		<br /><br />
+		<input type="submit" name="createSubmit" value="Reserve this Room"/> &nbsp;&nbsp;
 
-	<?php if (!isnull($reservationInfo)) { ?>
+		<?php if (!isnull($reservationInfo)) { ?>
 
-	<input type="submit" name="deleteSubmit" value="Delete" id="deleteReservation"/>
+		<input type="submit" name="deleteSubmit" value="Delete" id="deleteReservation"/>
 
-	<?php }	?>
+		<?php }	?>
 
-</form>
-
-
+	</form>
 
 
-<?php
-templates::display('footer');
-?>
+
+
+	<?php
+	templates::display('footer');
+	?>
