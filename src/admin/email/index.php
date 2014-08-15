233b6ed6584bc05de7c59edd80137cb728a32f52
Michael Bond <mbond@the-forgotten.org>
2014-08-15 09:22:16 -0400

    Added recipient information to display

--- src/admin/email/index.php
+++ src/admin/email/index.php
@@ -45,14 +45,43 @@ templates::display('header');
 
 <?php if (!$error) { ?>
 
-<form action="" method="post">
+<table class="table">
+	<tr>
+		<td>
+			<strong>User Name</strong>
+		</td>	
+		<td>
+			{local var="username"}
+		</td>
+	</tr>
+		<tr>
+		<td>
+			<strong>Group Name</strong>
+		</td>	
+		<td>
+			{local var="groupname"}
+		</td>
+	</tr>
+		<tr>
+		<td>
+			<strong>Email</strong>
+		</td>	
+		<td>
+			{local var="email"}
+		</td>
+	</tr>
+</table>
 
-<label for="subject">Email Subject: </label> <input type="text" name="subject" id="subject" />
+<form action="{phpself query="true"}" method="post">
+{csrf}
+<input type="hidden" name="id" value="{local var="id"}"/>
+
+<label for="subject"><strong>Email Subject:</strong></label> <input type="text" name="subject" id="subject" value="{local var="subject"}"style="width: 400px;" />
 
 <br />
 
-<label for="email">Email Message: </label><br />
-<textarea name="email" id="email"></textarea>
+<label for="email"><strong>Email Message:</strong></label><br />
+<textarea name="email" id="email" style="width: 500px; height: 300px;">{local var="emailMessage"}</textarea>
 <br /><br />
 <input type="submit" name="sendEmail" value="Send Email" />
 
