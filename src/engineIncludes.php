84e3674576f730f3cb33504b7fa3155e383241d3
Michael Bond <mbond@the-forgotten.org>
2014-08-15 07:13:03 -0400

    uncommented line to make migration to production easier.

--- src/engineIncludes.php
+++ src/engineIncludes.php
@@ -4,7 +4,7 @@ $databaseOptions = array(
 	'username' => 'username',
 	'password' => 'password'
 );
-// require_once('/home/www.libraries.wvu.edu/phpincludes/databaseConnectors/database.lib.wvu.edu.remote.php');
+require_once('/home/www.libraries.wvu.edu/phpincludes/databaseConnectors/database.lib.wvu.edu.remote.php');
 $databaseOptions['dbName'] = 'roomReservations';
 $db                        = db::create('mysql', $databaseOptions, 'appDB');
 
