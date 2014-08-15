4e0f8a0ad8bc46186fcd63de7d3babe097bfc277
Michael Bond <mbond@the-forgotten.org>
2014-08-14 20:01:26 -0400

    Store the optional email address for reservations

--- src/includes/class_reservation.php
+++ src/includes/class_reservation.php
@@ -572,7 +572,7 @@ class reservation {
 		}
 
 		if ($this->isNew()) {
-			$sql        = sprintf("INSERT INTO `reservations` (createdOn,createdBy,createdVia,roomID,startTime,endTime,modifiedOn,modifiedBy,username,initials,groupname,comments,seriesID) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)");
+			$sql        = sprintf("INSERT INTO `reservations` (createdOn,createdBy,createdVia,roomID,startTime,endTime,modifiedOn,modifiedBy,username,initials,groupname,comments,seriesID,email) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 			$sqlOptions = array(
 				time(),
 				session::get("username"),
@@ -586,7 +586,8 @@ class reservation {
 				$userInformation['initials'],
 				$groupname,
 				$comments,
-				(isnull($seriesID))?"":$seriesID
+				(isnull($seriesID))?"":$seriesID,
+				(isset($_POST['mysql']['notificationEmail']))?$_POST['HTML']['notificationEmail']:""
 				);
 		}
 		else {
