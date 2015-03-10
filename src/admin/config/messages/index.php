fc35f692e2e7c9e27288b9b9912ef1a4bb27ca05
Michael Bond <mbond@the-forgotten.org>
2015-03-10 14:12:24 -0400

    Convert admin messages page to use new class.

--- src/admin/config/messages/index.php
+++ src/admin/config/messages/index.php
@@ -2,76 +2,19 @@
 require_once("../../engineHeader.php");
 recurseInsert("includes/functions.php","php");
 
-function defineFields() {
-	$fields                             = array();
-	$fields['reservationCreated']       = getResultMessage('reservationCreated');
-	$fields['reservationUpdated']       = getResultMessage('reservationUpdated');
-	$fields['dataValidationError']      = getResultMessage('dataValidationError');
-	$fields['invalidUsername']          = getResultMessage('invalidUsername');
-	$fields['invalidDate']              = getResultMessage('invalidDate');
-	$fields['endBeforeStart']           = getResultMessage('endBeforeStart');
-	$fields['duplicateReservation']     = getResultMessage('duplicateReservation');
-	$fields['reservationConflict']      = getResultMessage('reservationConflict');
-	$fields['policyError']              = getResultMessage('policyError');
-	$fields['sameDayReservation']    	= getResultMessage('sameDayReservation');
-	$fields['systemsPolicyError']       = getResultMessage('systemsPolicyError');
-	$fields['maxFineExceeded']          = getResultMessage('maxFineExceeded');
-	$fields['multipleRoomBookings']     = getResultMessage('multipleRoomBookings');
-	$fields['patronReservationInfo']    = getResultMessage('patronReservationInfo');
-	$fields['policyLabel']              = getResultMessage('policyLabel');
-	$fields['libraryClose']             = getResultMessage('libraryClose');
-	$fields['reservationInPast']        = getResultMessage('reservationInPast');
-	$fields['reservationLengthTooLong'] = getResultMessage('reservationLengthTooLong');
-	$fields['userOverSystemHours']      = getResultMessage('userOverSystemHours');
-	$fields['userOverLibraryHours']     = getResultMessage('userOverLibraryHours');
-	$fields['userOverPolicyHours']      = getResultMessage('userOverPolicyHours');
-	$fields['userOverSystemBookings']   = getResultMessage('userOverSystemBookings');
-	$fields['userOverBuildingBookings'] = getResultMessage('userOverBuildingBookings');
-	$fields['userOverPolicyBookings']   = getResultMessage('userOverPolicyBookings');
-	$fields['errorInserting']           = getResultMessage('errorInserting');
-	$fields['tooFarInFuture']           = getResultMessage('tooFarInFuture');
-	$fields['emailNotProvided']         = getResultMessage('emailNotProvided');
-	
-	return($fields);
-}
+$messages = new messages;
 
-$fields = defineFields();
 
 if(isset($_POST['MYSQL']['sysconfig_submit'])) {
 
-	$error = FALSE;
+	$success = $messages->setMessages();
 
-	foreach ($fields as $name=>$value) {
+	if ($success) errorHandle::successMsg("All fields updated successfully");
 
-		if (is_empty($_POST['MYSQL'][$name])) {
-			errorHandle::errorMsg($name." left blank.");
-			$error = TRUE;
-		}
-
-		if ($error === FALSE) {
-			$fields[$name] = $_POST['MYSQL'][$name];
-
-			$db        = db::get($localvars->get('dbConnectionName'));
-			$sql       = sprintf("UPDATE `resultMessages` SET `value`=? WHERE `name`=?");
-			$sqlResult = $db->query($sql,array($_POST['MYSQL'][$name],$name));
-
-			if ($sqlResult->error()) {
-				errorHandle::newError($sqlResult->errorMsg(), errorHandle::DEBUG);
-				errorHandle::errorMsg($name." not updated correctly. Other fields may still be updated.");
-				$error = TRUE;
-			}
-
-		}
-	}
-
-	if ($error === FALSE) {
-		errorHandle::successMsg("All fields updated successfully");
-	}
-
-	$fields = defineFields();
 }
 
 $localvars->set("prettyPrint",errorHandle::prettyPrint());
+$localvars->set("messagesEditTable",$messages->buildEditTable());
 
 templates::display('header');
 ?>
@@ -86,22 +29,7 @@ templates::display('header');
 
 	{csrf}
 
-	<table>
-		<?php foreach($fields as $name=>$value) { ?>
-
-		<tr>
-			<td>
-				<label for="<?php print $name; ?>"><?php print $name; ?></label>
-			</td>
-			<td>
-
-				<textarea id="<?php print $name; ?>" name="<?php print $name; ?>"><?php print $value; ?></textarea>
-
-			</td>
-		</tr>
-
-		<?php }	?>
-	</table>
+	{local var="messagesEditTable"}
 
 	<input type="submit" name="sysconfig_submit" value="submit" />
 </form>
