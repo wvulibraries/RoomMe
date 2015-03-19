a8ecf903e7ed4867a308959d84e6be8db4d8f54c
Tim Broadwater <timbrdh2o@gmail.com>
2015-03-19 13:09:00 -0400

    Revert "Changed Template"
    This reverts commit f7b585e528d846bb18ef2ead7bb81170caac03d7.

--- src/engineHeader.php
+++ src/engineHeader.php
@@ -22,7 +22,7 @@ $messages = new messages;
 
 recurseInsert("includes/engineHeader.php","php");
 // templates::load("library2012.3col");
-templates::load("rooms2015");
+templates::load("library2014-backpage");
 
 $localvars->set("roomResBaseDir","/services/rooms");
 
