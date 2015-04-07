c26afa42165437a01f6ba85d638862200e28d928
Michael Bond <mbond@the-forgotten.org>
2015-04-07 08:49:58 -0400

    commented out old CSS

--- src/headerIncludes.php
+++ src/headerIncludes.php
@@ -1,7 +1,7 @@
-
+<!-- 
 <link rel="stylesheet" type="text/css" href="{local var="roomResBaseDir"}/css/reservations.less" />
 <script src="{local var="roomResBaseDir"}/javascript/jquery.simplemodal.1.4.4.min.js" type="text/javascript"></script>
-<script type="text/javascript" src="{local var="roomResBaseDir"}/javascript/doubleMultiSelect.js"></script>
+<script type="text/javascript" src="{local var="roomResBaseDir"}/javascript/doubleMultiSelect.js"></script> -->
 
 <script type="text/javascript">
 var roomReservationHome = '{local var="roomReservationHome"}';
