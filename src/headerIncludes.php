87ffbed45e264581ac822778f595dafb61c27db1
Michael Bond <mbond@the-forgotten.org>
2015-04-07 11:00:00 -0400

    number of columns
    This variable should be changed based on the width of the page.

--- src/headerIncludes.php
+++ src/headerIncludes.php
@@ -5,6 +5,7 @@
 
 <script type="text/javascript">
 var roomReservationHome = '{local var="roomReservationHome"}';
+var numberOfColumns     = 7;
 </script>
 
 <script type="text/javascript" src="{local var="roomResBaseDir"}/javascript/roomReservations.js"></script>
