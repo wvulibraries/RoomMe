a9fa691bd45d764dc295cd9650684ecd25cde931
Michael Bond <mbond@the-forgotten.org>
2015-04-15 06:58:14 -0400

    Generic login script
    A generic login script to setup a vagrant user session. This account has full admin access to the local vagrant box.

---
+++ src/vagrantLogin.php
@@ -0,0 +1,16 @@
+<?php 
+
+require 'engineHeader.php';
+
+$session = 'a:7:{s:4:"page";s:25:"/services/rooms/index.php";s:2:"qs";s:0:"";s:6:"groups";a:4:{i:0;s:32:"libraryWeb_roomReservation_rooms";i:1;s:32:"libraryWeb_roomReservation_admin";i:2;s:26:"libraryWeb_roomReservation";i:3;s:12:"Domain Users";}s:2:"ou";s:4:"Main";s:8:"username";s:7:"vagrant";s:8:"authtype";s:4:"ldap";s:9:"auth_ldap";a:3:{s:6:"groups";a:4:{i:0;s:95:"CN=libraryWeb_roomReservation_rooms,OU=Web,OU=Groups,OU=Library,OU=Main,DC=wvu-ad,DC=wvu,DC=edu";i:1;s:95:"CN=libraryWeb_roomReservation_admin,OU=Web,OU=Groups,OU=Library,OU=Main,DC=wvu-ad,DC=wvu,DC=edu";i:2;s:89:"CN=libraryWeb_roomReservation,OU=Web,OU=Groups,OU=Library,OU=Main,DC=wvu-ad,DC=wvu,DC=edu";i:3;s:48:"CN=Domain Users,CN=Users,DC=wvu-ad,DC=wvu,DC=edu";}s:6:"userDN";s:93:"CN=vagrant box,OU=systemsGeneratedCourtesyAccounts,OU=Library,OU=Main,DC=wvu-ad,DC=wvu,DC=edu";s:8:"username";s:7:"vagrant";}}'; 
+
+session::import(unserialize($session));
+
+?>
+
+<p>You are now logged in. Use the links below to navigate:</p>
+
+<ul>
+	<li><a href="/services/rooms/">Public Pages</a></li>
+	<li><a href="/services/rooms/admin">Admin Pages</a></li>
+</ul>
