387bdd0a377b2ec0191cd81e9cfb14ae5b42d500
Tim Broadwater <emailme@timbroadwater.com>
2015-04-17 09:20:01 -0400

    Rooms by Building #64

--- src/building/index.php
+++ src/building/index.php
@@ -23,9 +23,15 @@ $localvars->set("errors",errorHandle::prettyPrint());
 templates::display('header');
 ?>
 
-<header>
-<h3>{local var="libraryName"}</h3>
-</header>
+<h3 class="roomH3" style="display: inline-block;">{local var="libraryName"}</h3>
+
+<!-- Extra Links -->
+<a class="policyLink roomTabletDesktop" href="{local var="advancedSearch"}">Advanced Search <i class="fa fa-cog"></i></a>
+<a class="policyLink3 roomTabletDesktop" href="{local var="policiesPage"}">Reservation Policies 
+	<i class="fa fa-exclamation-circle"></i>
+</a>
+
+<hr class="roomHR roomTabletDesktop" />
 
 {local var="errors"}
 
@@ -50,25 +56,56 @@ templates::display('header');
 	</ul>
 
 <section id="reservationsBuildingRoomsList">
+	<h4>Rooms</h4>
+	<ul>
+	<?php foreach($buildingRooms as $I=>$room) { ?>
 
-	<header>
-		<h4>Rooms</h4>
-	</header>
+		<li class="whhp">
+			<a href="room/?room=<?php print htmlSanitize($room['ID']);?>"><?php if (!is_empty($room['pictureURL'])) {?>
+				<img src="<?php print htmlSanitize($room['pictureURL']);?>" class="buildingListPicture" alt="<?php print htmlSanitize($room['displayName']); ?>" /><span class="reservationsBuildingRoomsListName">
+					<?php } print htmlSanitize($room['displayName']); ?>
+				</span></a>
+		</li>
 
-<ul>
-<?php foreach($buildingRooms as $I=>$room) { ?>
-
-	<li>
-		<a href="room/?room=<?php print htmlSanitize($room['ID']);?>"><?php if (!is_empty($room['pictureURL'])) {?><img src="<?php print htmlSanitize($room['pictureURL']);?>" class="buildingListPicture" alt="<?php print htmlSanitize($room['displayName']); ?>" /><?php } print htmlSanitize($room['displayName']); ?></a>
-	</li>
-
-<?php } ?>
-</ul>
-
+	<?php } ?>
+	</ul>
 </section>
 
 <div id="calendarModal">
 </div>
+
+	<!-- Advanced Search -->
+	<div style="clear:both;"></div>
+	<hr class="roomHR roomMobile" />
+	<a href="{local var="advancedSearch"}" id="asbutton" class="bSubmit roomMobile"><i class="fa fa-cog"></i> Advanced Search</a>
+
+	<div class="clear:both;"></div>
+	<br>
+
+	<!-- Rooms Navigation -->
+	<h4 style="float:left;">Rooms by Building:</h4>
+	<hr class="roomHR"></hr>
+	<nobr><a class="policyLink1" href="/services/rooms/building/?building=2"><i class="fa fa-building"></i>Downtown Campus Library</a></nobr>
+	<nobr><a class="policyLink1" href="/services/rooms/building/?building=1"><i class="fa fa-building"></i>Evansdale Library</a></nobr>
+	<nobr><a class="policyLink1" href="http://home.hsc.wvu.edu/its/forms/library-study-room-reservation/" target="_blank"><i class="fa fa-building"></i>Health Sciences Library</a></nobr>
+	<hr class="roomHR"></hr>
+	<br>
+
+	<!-- Mobile UI -->			
+	<a class="policyLink roomMobile" href="{local var="policiesPage"}">Reservation Policies <i class="fa fa-exclamation-circle"></i></a>
+
+	<?php if (is_empty(session::get("username"))) { ?>
+		<a id="userLoginSubmit" href="{local var="loginURL"}" class="roomMobile bSubmit">
+			<i class="fa fa-user"></i> User Login
+		</a>
+	<?php } else { ?>
+		<a id="userLoginSubmit" href="{local var="roomReservationHome"}/calendar/user/" class="roomMobile bSubmit">
+			<i class="fa fa-check"></i> My Reservations
+		</a>
+		<a id="userLoginSubmit" href="{engine var="logoutPage"}?csrf={engine name="csrfGet"}" class="roomMobile bSubmit">
+			<i class="fa fa-user"></i> User Logout
+		</a>
+	<?php } ?>
 
 <?php
 templates::display('footer');
