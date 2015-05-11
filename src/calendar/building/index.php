7c9e803a2e5d8f0fc6372cc1e2d961799ee0b69b
Michael Bond <mbond@the-forgotten.org>
2015-05-11 09:35:18 -0400

    Builds the building calendars with the new methods

--- src/calendar/building/index.php
+++ src/calendar/building/index.php
@@ -1,35 +1,105 @@
 <?php
 require_once("../../engineHeader.php");
+recurseInsert("includes/functions.php","php");
 
-try {
-	$calendar = new calendar(FALSE);
-	$calendar->setLocalvars();
-	$localvars->set("dateSelect",$calendar->buildDateSelects());
-	$localvars->set("calendar",drawRoomCalendar($calendar->getRoomID(),array($calendar->dates['display']['month'],$calendar->dates['display']['day'],$calendar->dates['display']['year'])));
+// $localVars = localVars::getInstance();
+$db        = db::get($localvars->get('dbConnectionName'));
+
+$sql       = sprintf("SELECT * FROM building ORDER BY name");
+$sqlResult = $db->query($sql);
+
+$localvars->set("policyLabel",$messages->get("policyLabel"));
+
+$building = new building;
+
+if (isset($_POST['MYSQL']['library'])) {
+	$buildingID = $reservation->building['ID'];
 }
-catch (Exception $e) {
-	errorHandle::errorMsg("Invalid or missing Building or Room ID");
-	$error = TRUE;
+else if (isset($_GET['MYSQL']['building'])) {
+	$buildingID = $_GET['MYSQL']['building'];
+}
+else {
+	$buildingID = NULL;
 }
 
-templates::display('header');
+$localvars->set("buildingSelectOptions",$building->selectBuildingListOptions(FALSE,$buildingID));
+$localvars->set("buildingName",getBuildingName($buildingID));
+
+$date = new date;
+$localvars->set("monthSelect",$date->dropdownMonthSelect(1,TRUE,array("id"=>"start_month_modal")));
+$localvars->set("daySelect",$date->dropdownDaySelect(TRUE,array("id"=>"start_day_modal")));
+$localvars->set("yearSelect",$date->dropdownYearSelect(0,1,TRUE,array("id"=>"start_year_modal")));
+
+$localvars->set("headerDate",date("l, F j"));
+
+templates::display('header'); 
 ?>
 
-<header>
-	<h1>{local var="name"} Calendar</h1>
-	<h2>{local var="month"} / {local var="day"} / {local var="year"}</h2>
-</header>
+	<!-- Reservations Section -->
+	<h3 class="roomH3 roomMobile" style="margin-top: 20px;">Available Now</h3>
+	<hr class="roomHR roomMobile" />
+	<ul id="mobileList" class="roomMobile"></ul>
 
-<br />
+	<h3 class="roomH3 roomTabletDesktop">Calendar for {local var="buildingName"} on <span class="currentDay" id="headerDate">{local var="headerDate}</span></h3>
 
-<section>
+	<hr class="roomHR roomTabletDesktop" />
+	<div class="styled-select roomTabletDesktop">
+		<select id="building_modal">
+			{local var="buildingSelectOptions"}             
+		</select>
+	</div>
+	<div class="styled-select roomTabletDesktop">
+		{local var="monthSelect"}
+	</div>                                          
+	<div class="styled-select roomTabletDesktop">
+		{local var="daySelect"}
+	</div>
+	<div class="styled-select roomTabletDesktop">
+		{local var="yearSelect"} 
+	</div>
+	<a id="calUpdateFormSubmit" class="bSubmit roomTabletDesktop">
+		<i class="fa fa-calendar"></i> Find A Room
+	</a>
+	<div style="clear:both;"></div>
+	<h3 class="roomH3 roomMobile" style="margin-top: 40px;">Future Dates</h3>
+	<hr class="roomHR roomMobile" />
+	<a href="{local var="advancedSearch"}" id="asbutton" class="bSubmit roomMobile"><i class="fa fa-cog"></i> Advanced Search</a>
 
-	{local var="dateSelect"}
-	<br /><br />
-	{local var="calendar"}
+	<!-- Table Pager -->
+	<div class="tablePager roomTabletDesktop">
+		<a class="pagerButtons"><i id="pagerFirst" data-startCols="0" data-endCols="7" class="fa fa-step-backward pagerLink"></i></a>
+		<a class="pagerButtons"><i id="pagerPrev"  data-startCols="" data-endCols="" class="fa fa-backward pagerLink"></i></a>
+		<a class="pagerButtons"><i id="pagerNext"  data-startCols="7" data-endCols="14"class="fa fa-forward pagerLink"></i></a>
+		<a class="pagerButtons"><i id="pagerLast"  data-startCols="" data-endCols="" class="fa fa-step-forward pagerLink" ></i></a>
+	</div>
 
-</section>
+	<!-- Calendar Call -->
+	<table id="reservationsRoomTable" cellspacing="0" cellpadding="0">
+		<thead>
+			<tr id="reservationsRoomTableHeaderRow">			
+			</tr>
+		</thead>
+		<tbody id="reservationsRoomTableBody">
 
-<?php
-templates::display('footer');
-?>
+		</tbody>
+	</table>
+
+	<div class="clear:both;"></div>
+
+	<!-- Mobile UI -->			
+	<a class="policyLink roomMobile" href="{local var="policiesPage"}">Reservation Policies <i class="fa fa-exclamation-circle"></i></a>
+
+	<?php if (is_empty(session::get("username"))) { ?>
+		<a id="userLoginSubmit" href="{local var="loginURL"}" class="roomMobile bSubmit">
+			<i class="fa fa-user"></i> User Login
+		</a>
+	<?php } else { ?>
+		<a id="userLoginSubmit" href="{local var="roomReservationHome"}/calendar/user/" class="roomMobile bSubmit">
+			<i class="fa fa-check"></i> My Reservations
+		</a>
+		<a id="userLoginSubmit" href="{engine var="logoutPage"}?csrf={engine name="csrfGet"}" class="roomMobile bSubmit">
+			<i class="fa fa-user"></i> User Logout
+		</a>
+	<?php } ?>
+
+<?php templates::display('footer'); ?>
