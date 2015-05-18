b329cada9d89b448feedaab35da747952044ed05
Tim Broadwater <emailme@timbroadwater.com>
2015-05-18 09:11:31 -0400

    Generic Template

---
+++ serverConfiguration/genericTemplate/templateFooter.php
@@ -0,0 +1,11 @@
+             </section>
+		</div>
+
+		<!-- Footer -->
+    	<footer id="footer">
+		    <div class="wrap">
+		        <!-- Your website footer information goes here -->
+	        </div>
+		</footer>
+    </body>
+</html>                
