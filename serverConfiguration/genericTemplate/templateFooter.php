d785a6f0473707c7e03159ceb1d6a00e9bb2a603
Tim Broadwater <emailme@timbroadwater.com>
2015-05-19 08:55:56 -0400

    URL Change

--- serverConfiguration/genericTemplate/templateFooter.php
+++ serverConfiguration/genericTemplate/templateFooter.php
@@ -5,7 +5,7 @@
     	<footer id="footer">
 		    <div class="wrap">
 		        <!-- Your website footer information goes here -->
-		        <p style="text-align: center; font-size: 0.75em;">Powered by <em><a href="https://github.com/wvulibraries/Room-Reservations">RoomMe</a></em> a product of <a href="https://lib.wvu.edu">WVU Libraries</a>.</p>
+		        <p style="text-align: center; font-size: 0.75em;">Powered by <em><a href="https://github.com/wvulibraries/RoomMe">RoomMe</a></em> a product of <a href="https://lib.wvu.edu">WVU Libraries</a>.</p>
 		        <br>
 	        </div>
 		</footer>
