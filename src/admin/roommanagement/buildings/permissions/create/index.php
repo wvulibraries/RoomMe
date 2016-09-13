abf40dbc69b38dc9e875b3aef246194e1439944b
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2016-09-13 15:56:54 -0400

    Updates for Permissions Create Form (wip)
    additional updates for the form. Currently now working.

--- src/admin/roommanagement/buildings/permissions/create/index.php
+++ src/admin/roommanagement/buildings/permissions/create/index.php
@@ -1,35 +1,98 @@
 <?php
 // @TODO This file is a mess. It is in need of a refactoring/cleanup
 //
-require_once("../../../engineHeader.php");
+require_once("../../../../engineHeader.php");
+
+
 
 $errorMsg = "";
 $error    = FALSE;
 
 $db       = db::get($localvars->get('dbConnectionName'));
 
+// we are editing a reservation
+$reservationInfo = NULL;
 $resourceID      = "";
 $resourceType    = "";
 $username        = "";
 $submitError     = FALSE;
 
-// Set some localvars for use in the HTML below.
-$localvars->set("resourceID",($reservation->isNew())?"":$reservation->reservation['resourceID']);
-$localvars->set("resourceType",($reservation->isNew())?"":$reservation->reservation['resourceType']);
-$localvars->set("username",($reservation->isNew())?"":$reservation->reservation['username']);
+$reservationPermissions = new reservationPermissions;
+//$reservation = new Reservation;
 
+// Check to see if we want to create a new reservation with the patron information
+// we want to make sure the patron information remains, but all the reservation information is removed ... as if it is a new reservation.
+// if (isset($_POST['MYSQL']['createNewFromOld'])) {
+//
+// 	unset($_GET['MYSQL']['id']);
+// 	unset($_POST['MYSQL']['reservationID']);
+//
+// }
+
+// try {
+
+	// Is this an Update?
+	// Currently checking for this in both get and post.
+	// if (isset($_GET['MYSQL']['id']) || (isset($_POST['MYSQL']['reservationID']) && !is_empty($_POST['MYSQL']['reservationID'])) ) {
+	//
+	// 	$reservationID = (isset($_POST['MYSQL']['reservationID']) && !is_empty($_POST['MYSQL']['reservationID']))?$_POST['MYSQL']['reservationID']:$_GET['MYSQL']['id'];
+	//
+	// 	if ($reservation->get($reservationID) === FALSE) {
+	// 		throw new Exception("Error retrieving reservation.");
+	// 	}
+	//
+	// }
+
+
+// 	if (isset($_POST['MYSQL']['createSubmit'])) {
+//
+//
+// 		$reservation->setBuilding($_POST['MYSQL']['library']);
+// 		$reservation->setRoom($_POST['MYSQL']['room']);
+//
+// 		if (!$reservation->create()) {
+// 			throw new Exception("Error Creating Reservation.");
+// 		}
+//
+// 	}
+// 	else if (isset($_POST['MYSQL']['deleteSubmit'])) {
+//
+// 		if (!$reservation->delete()) {
+// 			throw new Exception("Error deleting reservation.");
+// 		}
+//
+// 		// @TODO this should not be hard coded.
+// 		header('Location: ../list/');
+//
+// 	}
+//
+// }
+// catch(Exception $e) {
+// 	errorHandle::errorMsg($e->getMessage());
+//
+// 	// Setup to repopulate form on error
+// 	$submitError = TRUE;
+// }
+
+// Set some localvars for use in the HTML below.
+// $localvars->set("resourceID",($reservation->isNew())?"":$reservation->reservation['resourceID']);
+// $localvars->set("resourceType",($reservation->isNew())?"":$reservation->reservation['resourceType']);
+// $localvars->set("username",($reservation->isNew())?"":$reservation->reservation['username']);
+
+
+// Check to see if we want to create a new reservation with the patron information
+// we want to make sure the patron information remains, but all the reservation information is removed ... as if it is a new reservation.
 if (isset($_POST['MYSQL']['createNewFromOld'])) {
-	$localvars->set("resourceID", (isset($_POST['HTML']['resourceID']) && !is_empty($_POST['HTML']['resourceID']))?$_POST['HTML']['resourceID']:"");
-	$localvars->set("resourceType", (isset($_POST['HTML']['resourceType']) && !is_empty($_POST['HTML']['resourceType']))?$_POST['HTML']['resourceType']:"");
+
+	$localvars->set("resourceID",(isset($_POST['HTML']['resourceID']) && !is_empty($_POST['HTML']['resourceID']))?$_POST['HTML']['resourceID']:"");
+	$localvars->set("resourceType",(isset($_POST['HTML']['resourceType']) && !is_empty($_POST['HTML']['resourceType']))?$_POST['HTML']['resourceType']:"");
 	$localvars->set("username", (isset($_POST['HTML']['username']) && !is_empty($_POST['HTML']['username']))?$_POST['HTML']['username']:"");
 }
 
 if ($submitError) {
-
 	$localvars->set("resourceID",$_POST['HTML']['resourceID']);
 	$localvars->set("resourceType",$_POST['HTML']['resourceType']);
 	$localvars->set("username",$_POST['HTML']['username']);
-
 }
 
 templates::display('header');
@@ -39,28 +102,26 @@ templates::display('header');
 	<h1>{local var="action"} a Permission</h1>
 </header>
 
-<?php if (count($engine->errorStack) > 0) {	?>
-<section id="actionResults">
-	<header>
+<?php //if (count($engine->errorStack) > 0) {	?>
+<!-- <section id="actionResults"> -->
+	<!-- <header>
 		<h1>Results</h1>
-	</header>
-	<?php print errorHandle::prettyPrint(); ?>
-</section>
-<?php } ?>
+	</header> -->
+	<?php //print errorHandle::prettyPrint(); ?>
+<!-- </section> -->
+<?php //} ?>
 
 	<form action="{phpself query="true"}" method="post">
 		{csrf}
 
-		<input type="hidden" name="reservationID" value="{local var="reservationID"}" />
+		<input type="hidden" name="ID" value="{local var="ID"}" />
 
 		<fieldset>
-			<legend>User Information</legend>
-			<label for="resourceID" class="requiredField">resourceID:</label> &nbsp; <input type="text" id="resourceID" name="resourceID" value="{local var="resourceID"}" required/>
+			<legend>Permission Information</legend>
+			<label for="resourceID" class="requiredField">Resource ID:</label> &nbsp; <input type="text" id="resourceID" name="resourceID" value="{local var="resourceID"}" required/>
 			<br />
-			<legend>User Information</legend>
-			<label for="resourceType" class="requiredField">resourceType:</label> &nbsp; <input type="text" id="resourceType" name="resourceType" value="{local var="resourceType"}" required/>
+			<label for="resourceType" class="requiredField">Resource Type:</label> &nbsp; <input type="text" id="resourceType" name="resourceType" value="{local var="resourceType"}" required/>
 			<br />
-			<legend>User Information</legend>
 			<label for="username" class="requiredField">Username:</label> &nbsp; <input type="text" id="username" name="username" value="{local var="username"}" required/>
 			<br />
 		</fieldset>
