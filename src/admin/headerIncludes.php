cbcf268ea7eb36b276a2a6cb6d9227f8cc45010a
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2016-09-22 14:30:12 -0400

    updated headerincludes
    Updated header includes link to dayCheck.js and additional css to fix the tables that were overflowing the container by adding scrollbars.

--- src/admin/headerIncludes.php
+++ src/admin/headerIncludes.php
@@ -11,6 +11,26 @@
   <script type="text/javascript">
   	var roomReservationHome = '{local var="roomReservationHome"}';
   </script>
-  
+
 <script type="text/javascript" src="{local var="roomResBaseDir"}/javascript/roomReservations.js"></script>
 <script type="text/javascript" src="{local var="roomResBaseDir"}/javascript/doubleMultiSelect.js"></script>
+<script type="text/javascript" src="{local var="roomResBaseDir"}/javascript/dayCheck.js"></script>
+
+
+<style>
+
+  .listTable {overflow:auto;}
+
+  .editTable {overflow:auto;}
+
+	::-webkit-scrollbar {
+    -webkit-appearance: none;
+    width: 4px;
+	}
+
+	::-webkit-scrollbar-thumb {
+    border-radius: 4px;
+    background-color: rgba(0,0,0,.5);
+    -webkit-box-shadow: 0 0 1px rgba(255,255,255,.5);
+  }
+</style>
