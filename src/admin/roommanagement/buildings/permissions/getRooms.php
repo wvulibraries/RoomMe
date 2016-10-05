08f23657c5264c35d5dac7cc4ee8f4757c81c0d6
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2016-10-05 11:24:52 -0400

    rooms.js & getRooms.php
    Files used to populate the dropdowns for only valid rooms for the specified building.

---
+++ src/admin/roommanagement/buildings/permissions/getRooms.php
@@ -0,0 +1,10 @@
+<?php
+		require_once("../../../../engineHeader.php");
+
+		$reservationPermissions = new reservationPermissions;
+
+		$building = isset($_GET['MYSQL']['building']) ? $_GET['MYSQL']['building'] : null;
+
+		header('Content-Type: application/json');
+		print (json_encode($reservationPermissions->getRooms($building)));
+?>
---
+++ src/javascript/rooms.js
@@ -0,0 +1,50 @@
+$(function(){
+  var resourceID;
+  var resourceType;
+  $('body').on('change', '.resourceID', function(){
+    resourceID = $('.resourceID').val();
+    $.ajax({
+      type: "GET",
+      url: '../getRooms.php?building='+resourceID,
+      success : function(data) {
+        /* Remove all options from the select list */
+        $('.rooms').empty();
+
+        var options;
+        options += '<option value="' + 'NULL' + '">' + 'Select a Room' + '</option>';
+        for (var i = 0; i < data.length; i++) {
+          options += '<option value="' + data[i].ID + '">' + data[i].name + '</option>';
+        }
+
+        $('.rooms').html(options);
+      },
+      error: function() {
+         console.log('failure');
+      },
+     });
+  });
+
+  $('body').on('change', '.resourceType', function(){
+    resourceType = $('.resourceType').val();
+    resourceID = $('.resourceID').val();
+
+    if ((resourceType == 0) || (resourceID == 'NULL')){
+      $('.rooms').prop("disabled", true);
+    } else {
+      $('.rooms').prop("disabled", false);
+    }
+
+    var visible = $('.resourceType option[value="1"]').is(':hidden');
+    if (visible) {
+      $('.resourceType option[value="1"]').hide();
+    }
+
+    var visible = $('.resourceType option[value="2"]').is(':hidden');
+    if (visible) {
+      $('.resourceType option[value="2"]').hide();
+    }
+
+  });
+
+  $("select.resourceType").change();
+});
