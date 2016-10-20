9d5713b37317237273af779d547ebe4fe7c4f582
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2016-10-20 14:01:48 -0400

    Updated sql
    Added field in the table.

--- sqlFiles/migrations/2016.08.08.1121.sql
+++ sqlFiles/migrations/2016.08.08.1121.sql
@@ -3,9 +3,11 @@ CREATE TABLE `reservePermissions` (
 	`ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
 	`resourceID` int(10) unsigned DEFAULT NULL,
 	`resourceType` varchar(30) DEFAULT NULL,
+	`roomID` int(10) unsigned DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
 	PRIMARY KEY (`ID`),
   KEY `resourceID` (`resourceID`),
   KEY `resourceType` (`resourceType`),
+	KEY `roomID` (`roomID`),
   KEY `email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
